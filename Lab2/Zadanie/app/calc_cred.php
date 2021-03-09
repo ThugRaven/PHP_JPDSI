ffe4fd029c9ace2fc76bd13eb29fb9046051f009
ThugRaven <kamilo1715@wp.pl>
2021-03-09 13:44:54 +0100

    Reformat calc_cred.php

--- Lab2/Zadanie/app/calc_cred.php
+++ Lab2/Zadanie/app/calc_cred.php
@@ -1,40 +1,40 @@
 <?php
 require_once dirname(__FILE__).'/../config.php';
 
-$amount = $_REQUEST ['amount'];
-$years = $_REQUEST ['years'];
-$interest = $_REQUEST ['interest'];
+$amount = $_REQUEST['amount'];
+$years = $_REQUEST['years'];
+$interest = $_REQUEST['interest'];
 
 if (!(isset($amount) && isset($years) && isset($interest))) {
-	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
+	$messages[] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
 }
 
 if ($amount == ""){
-	$messages [] = 'Nie podano kwoty';
+	$messages[] = 'Nie podano kwoty';
 }
 if ($years == ""){
-	$messages [] = 'Nie podano liczby lat';
+	$messages[] = 'Nie podano liczby lat';
 }
 if ($interest == ""){
-	$messages [] = 'Nie podano oprocentowania';
+	$messages[] = 'Nie podano oprocentowania';
 }
 
-if (empty( $messages )) {
+if (empty($messages)) {
 	if (!is_numeric($amount)){
-		$messages [] = 'Pierwsza wartość nie jest liczbą całkowitą';
+		$messages[] = 'Pierwsza wartość nie jest liczbą całkowitą';
 	}
 	
 	if (!is_numeric($years)){
-		$messages [] = 'Druga wartość nie jest liczbą całkowitą';
+		$messages[] = 'Druga wartość nie jest liczbą całkowitą';
 	}
         
         if (!is_numeric($interest)){
-		$messages [] = 'Trzecia wartość nie jest liczbą całkowitą';
+		$messages[] = 'Trzecia wartość nie jest liczbą całkowitą';
 	}
 }
 
 
-if (empty ($messages)) {
+if (empty($messages)) {
 	$amount = intval($amount);
 	$years = intval($years);
         $interest = intval($interest);
