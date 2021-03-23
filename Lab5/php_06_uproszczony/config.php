43bd03372940806728221c2d7bd92281257af651
ThugRaven <kamilo1715@wp.pl>
2021-03-23 13:27:31 +0100

    Add php_06_uproszczony

--- Lab5/php_06_uproszczony/config.php
+++ Lab5/php_06_uproszczony/config.php
@@ -6,6 +6,8 @@ $conf = new Config();
 $conf->root_path = dirname(__FILE__);
 $conf->server_name = 'localhost:80';
 $conf->server_url = 'http://'.$conf->server_name;
-$conf->app_root = '/PHP/Lab5/php_05_uproszczony';
+$conf->app_root = '/PHP/Lab5/php_06_uproszczony';
 $conf->app_url = $conf->server_url.$conf->app_root;
+$conf->action_root = $conf->app_root.'/app/ctrl.php?action=';
+$conf->action_url = $conf->server_url.$conf->action_root;
 ?>
